288a7b78ee43966029a64458beed0adbd8c156cc
OC888 <data666@yeah.net>
2016-09-10 12:34:04 +0800

    add new files

---
+++ TextfieldTest/PullTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  PullTestViewController.h
+//  TextfieldTest
+//
+//  Created by aaa on 16/9/10.
+//  Copyright © 2016年 WL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PullTestViewController : UIViewController
+
+@end
---
+++ TextfieldTest/PullTestViewController.m
@@ -0,0 +1,38 @@
+//
+//  PullTestViewController.m
+//  TextfieldTest
+//
+//  Created by aaa on 16/9/10.
+//  Copyright © 2016年 WL. All rights reserved.
+//
+
+#import "PullTestViewController.h"
+
+@interface PullTestViewController ()
+
+@end
+
+@implementation PullTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
